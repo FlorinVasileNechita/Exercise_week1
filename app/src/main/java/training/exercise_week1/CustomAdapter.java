74136e08b088900e73c8046dca912d8eca3c5e0e
nechitafv <nechitafv@gmail.com>
2017-01-23 01:16:01 +0000

    custom adapter in progress

---
+++ app/src/main/java/training/exercise_week1/CustomAdapter.java
@@ -0,0 +1,20 @@
+package training.exercise_week1;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+
+public class CustomAdapter {
+
+//    https://www.youtube.com/watch?list=PL6gx4Cwl9DGBsvRxJJOzG4r4k_zLKrnxl&v=nOdSARCVYic
+
+//    public CustomAdapter(Context context){
+//
+//    }
+
+
+
+
+
+}
+
--- app/src/main/java/training/exercise_week1/ListOfNotes.java
+++ app/src/main/java/training/exercise_week1/ListOfNotes.java
@@ -119,5 +119,4 @@ public class ListOfNotes extends AppCompatActivity {
         listView.setAdapter(adapter);
     }
 
-
 }

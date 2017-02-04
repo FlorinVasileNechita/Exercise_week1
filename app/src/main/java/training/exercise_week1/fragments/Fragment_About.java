c5d3b32239d7bb92ca69a0315552ad216c776ac4
FlorinVasileNechita <nechitafv@gmail.com>
2017-02-05 01:55:08 +0200

    About - WebView done

--- app/src/main/java/training/exercise_week1/fragments/Fragment_About.java
+++ app/src/main/java/training/exercise_week1/fragments/Fragment_About.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import training.exercise_week1.R;
 
@@ -14,13 +16,20 @@ import training.exercise_week1.R;
  * Created by florinnechita on 01/02/17.
  */
 
-public class Fragment_About extends Fragment{
+public class Fragment_About extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_about, container,false);
-        Log.d("TAG","1233333333333");
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        Log.d("TAG", "1233333333333");
+
+        WebView webView = (WebView) view.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://google.ro");
+
         return view;
     }
+
+
 }
